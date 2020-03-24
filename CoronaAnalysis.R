070b5635b759605ccaae6d7cc73c61ad4b57e0f1
andrew.reilly19 <andrew.reilly19@ncf.edu>
2020-03-24 18:01:08 -0400

    Added code to match the data, fix some of the counties so they match up, and then graph the data onto the map of florida.

--- CoronaAnalysis.R
+++ CoronaAnalysis.R
@@ -2,3 +2,25 @@ mygitpath <- 'https://raw.githubusercontent.com/bklingen/DataViz2020_Maps/master
 library(tidyverse)
 corona <- read_csv(mygitpath)
 corona
+
+library(maps)
+
+counties <- map_data("county")
+counties <- filter(counties, region=="florida")
+
+coronacount <- corona %>% count(County)
+countycount <- counties %>% count(subregion)
+
+coronacount$County <- tolower(coronacount$County)
+
+#compare <- countycount %>% full_join(coronacount, by=c("subregion"="County"))
+coronacount[11,1] <- "miami-dade" 
+coronacount[39,1] <- "st johns"
+coronacount[40,1] <- "st lucie"
+
+corona2 <- counties %>% left_join(coronacount, by=c("subregion"="County"))
+
+ggplot(corona2, aes(x = long, y = lat, group = group, fill = n)) + 
+  geom_polygon(color = "black", size = 0.5) + theme_minimal() +
+  scale_fill_viridis_c() +
+  labs(title="Coronavirus Cases in Florida, by County", fill="Number of Cases")
