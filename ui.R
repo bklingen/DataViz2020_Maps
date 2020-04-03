d8066a725ca80cece29b95f0bd94e5518e7ad2e7
jmhobbs29 <36276446+jmhobbs29@users.noreply.github.com>
2020-04-02 23:57:26 -0400

    Add files via upload
    server and ui files for corona virus map of Florida

---
+++ ui.R
@@ -0,0 +1,33 @@
+library(shiny)
+library(plotly)
+library(lubridate)
+library(dplyr)
+library(shinydashboard)
+
+coronaFL <- read_csv('https://raw.githubusercontent.com/bklingen/DataViz2020_Maps/Jamie/CoronaCases.csv')
+
+
+dashboardPage(
+  dashboardHeader(title = "Florida's Corona Virus Situation"),
+  dashboardSidebar(collapsed = TRUE),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(width=6, 
+          status="info", 
+          title="Total Number of Reported Cases by County",
+          solidHeader = TRUE,
+          plotOutput("FLmap")
+      ),
+      box(width=6, 
+          status="warning", 
+          title = "Top 5 Counties",
+          solidHeader = TRUE, 
+          collapsible = TRUE, 
+          footer="Read Remotely from File",
+          tableOutput("mydata")
+      )
+    )
+  )
+)
+
